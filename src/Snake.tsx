a8e78a3b59ec3816b0c29bb56d6561c041f99393
Patrick Miller <paddymills@proton.me>
2025-04-14 22:23:26 -0400

    start snake

--- src/Snake.tsx
+++ src/Snake.tsx
@@ -1,8 +1,52 @@
+import { useState } from "react";
+import { Button, Modal, ModalDialog, ModalClose, Typography } from "@mui/joy";
+
+type Loc = {
+  row: number;
+  col: number;
+};
+
+type Turn = {
+  loc: Loc;
+  dir: string;
+};
+
+type Snake = {
+  length: number;
+  head: Loc;
+  tail: Loc;
+  turns: Turn[];
+  update: () => Snake;
+};
+
 export function SnakeGame() {
+  const [board, setBoard] = useState(Array(20 * 10).fill(null));
+  // const [direction, setDirection] = useState("right");
+  const [modalText, setModalText] = useState<string | null>(null);
+
+  const restart = () => {
+    setBoard(Array(20 * 10).fill(null));
+    setModalText(null);
+  };
+
   return (
-    <div>
-      <h1>Snake</h1>
-      <p>...not implemented yet</p>
-    </div>
+    <>
+      <div className="game">
+        <Button onClick={restart}>Restart</Button>
+        <div className="board snake">
+          {board.map((cell, index) => (
+            <div key={index} className="snake-cell">
+              {cell}
+            </div>
+          ))}
+        </div>
+      </div>
+      <Modal open={modalText !== null} onClose={() => setModalText(null)}>
+        <ModalDialog>
+          <ModalClose />
+          <Typography>{modalText}</Typography>
+        </ModalDialog>
+      </Modal>
+    </>
   );
 }
