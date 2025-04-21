bfb89be728bc69999e30a9349935cc063e0f0e38
Patrick Miller <paddymills@proton.me>
2025-04-20 21:56:49 -0400

    workable snake

--- src/Snake.tsx
+++ src/Snake.tsx
@@ -1,43 +1,183 @@
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { Button, Modal, ModalDialog, ModalClose, Typography } from "@mui/joy";
+
+// movements per second
+const SPEED = 2;
 
 type Loc = {
   row: number;
   col: number;
 };
 
-type Turn = {
-  loc: Loc;
-  dir: string;
+type Snake = Loc[];
+
+enum Direction {
+  UP = "up",
+  DOWN = "down",
+  LEFT = "left",
+  RIGHT = "right",
+}
+
+const REVERSE_MAP = {
+  [Direction.UP]: Direction.DOWN,
+  [Direction.DOWN]: Direction.UP,
+  [Direction.LEFT]: Direction.RIGHT,
+  [Direction.RIGHT]: Direction.LEFT,
 };
 
-type Snake = {
-  length: number;
-  head: Loc;
-  tail: Loc;
-  turns: Turn[];
-  update: () => Snake;
-};
+function initSnake(): Snake {
+  return [
+    { row: 3, col: 5 },
+    { row: 3, col: 4 },
+    { row: 3, col: 3 },
+  ];
+}
 
 export function SnakeGame() {
-  const [board, setBoard] = useState(Array(20 * 10).fill(null));
-  // const [direction, setDirection] = useState("right");
+  const [direction, setDirection] = useState(Direction.RIGHT);
   const [modalText, setModalText] = useState<string | null>(null);
+  const [snake, setSnake] = useState<Snake>(initSnake());
+  const [playing, setPlaying] = useState(true);
+  const [food, setFood] = useState<Loc>({ row: 5, col: 10 });
+
+  const board = () => {
+    const board = Array(10 * 20).fill("snake-cell");
+    snake.forEach((loc) => {
+      board[loc.row * 20 + loc.col] = "snake-body";
+    });
+    board[food.row * 20 + food.col] = "snake-food";
+
+    return board;
+  };
+
+  const handleKeyPress = useCallback(
+    (event: KeyboardEvent) => {
+      // make sure not modifier keys are down
+      if (event.ctrlKey || event.altKey || event.metaKey || event.shiftKey)
+        return;
+
+      const turn = (newDirection: Direction) => {
+        // - check that turn is not 180 or the same direction
+        // - update turn
+        const [dirA, dirB] = [direction, newDirection].sort();
+        if (dirA === dirB || REVERSE_MAP[dirA] === dirB) return;
+
+        setDirection(newDirection);
+      };
+
+      // movement is arrow keys, WASD and HJKL(vim)
+      switch (event.key) {
+        case "ArrowUp":
+        case "w":
+        case "k":
+          turn(Direction.UP);
+          break;
+        case "ArrowDown":
+        case "s":
+        case "j":
+          turn(Direction.DOWN);
+          break;
+        case "ArrowLeft":
+        case "a":
+        case "h":
+          turn(Direction.LEFT);
+          break;
+        case "ArrowRight":
+        case "d":
+        case "l":
+          turn(Direction.RIGHT);
+          break;
+      }
+    },
+    [direction],
+  );
 
   const restart = () => {
-    setBoard(Array(20 * 10).fill(null));
+    setSnake(initSnake());
+    setDirection(Direction.RIGHT);
+    setPlaying(true);
     setModalText(null);
   };
+
+  const move = useCallback(() => {
+    // - add head in direction
+    // - remove tail if not(justAte)
+    // - set justAte = false
+
+    if (!playing) return;
+
+    const newSnake = snake.slice();
+
+    // copy head
+    newSnake.unshift(JSON.parse(JSON.stringify(newSnake[0])));
+
+    switch (direction) {
+      case Direction.UP:
+        newSnake[0].row -= 1;
+        break;
+      case Direction.DOWN:
+        newSnake[0].row += 1;
+        break;
+      case Direction.LEFT:
+        newSnake[0].col -= 1;
+        break;
+      case Direction.RIGHT:
+        newSnake[0].col += 1;
+        break;
+    }
+
+    // handle growth/movement
+    if (newSnake[0].row === food.row && newSnake[0].col === food.col) {
+      let newFood: Loc;
+      do {
+        newFood = {
+          row: Math.floor(Math.random() * 10),
+          col: Math.floor(Math.random() * 20),
+        };
+      } while (
+        newSnake.some(
+          (segment) =>
+            segment.row === newFood.row && segment.col === newFood.col,
+        )
+      );
+
+      setFood(newFood);
+    } else {
+      newSnake.pop();
+    }
+
+    // check for game over
+    if (
+      newSnake[0].row < 0 ||
+      newSnake[0].row >= 10 ||
+      newSnake[0].col < 0 ||
+      newSnake[0].col >= 20
+    ) {
+      setPlaying(false);
+      setModalText("Game Over");
+      return;
+    }
+
+    setSnake(newSnake);
+  }, [direction, snake, playing, food]);
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyPress);
+    return () => window.removeEventListener("keydown", handleKeyPress);
+  }, [handleKeyPress]);
+
+  useEffect(() => {
+    const intervalId = setInterval(move, 1000 / SPEED);
+    return () => clearInterval(intervalId);
+  }, [move]);
 
   return (
     <>
       <div className="game">
         <Button onClick={restart}>Restart</Button>
         <div className="board snake">
-          {board.map((cell, index) => (
-            <div key={index} className="snake-cell">
-              {cell}
-            </div>
+          {board().map((cell, index) => (
+            <div key={index} className={cell}></div>
           ))}
         </div>
       </div>
