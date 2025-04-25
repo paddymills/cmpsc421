f4200b1c170ca6d631dd6ddaca558403bda93977
Patrick Miller <paddymills@proton.me>
2025-04-24 22:26:10 -0400

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,15 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import { Button, ToggleButtonGroup } from "@mui/joy";
-import { TicTacToeGame } from "./TicTacToe";
-import { SnakeGame } from "./Snake";
+import { Outlet } from "react-router";
+import { NavLink } from "react-router";
 
 function App() {
-  const [game, setGame] = useState<string>(
-    localStorage.getItem("game") || "TicTacToe",
-  );
-
-  useEffect(() => {
-    localStorage.setItem("game", game);
-  }, [game]);
-
-  let gameBoard;
-  if (game === "TicTacToe") {
-    gameBoard = <TicTacToeGame />;
-  } else if (game === "Snake") {
-    gameBoard = <SnakeGame />;
-  }
-
   return (
     <>
-      <ToggleButtonGroup
-        color="primary"
-        variant="soft"
-        value={game}
-        onChange={(_event, newValue) => setGame(newValue || game)}
-      >
-        <Button value="TicTacToe">TicTacToe</Button>
-        <Button value="Snake">Snake</Button>
-      </ToggleButtonGroup>
-      {gameBoard}
+      <nav>
+        <NavLink to="/tictactoe">Tic Tac Toe</NavLink>
+        <NavLink to="/snake">Snake</NavLink>
+      </nav>
+      <Outlet />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
+import { TicTacToeGame } from "./TicTacToe.tsx";
+import { SnakeGame } from "./Snake.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="tictactoe" element={<TicTacToeGame />} />
+          <Route path="snake" element={<SnakeGame />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
